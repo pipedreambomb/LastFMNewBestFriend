45ccc001b73a694653244c8ff8042b43f0e844ac
George Nixon <g@georgenixon.co.uk>
2011-04-25 22:52:33 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LastFMBestFriend::Application.config.secret_token = '9b6ee2d6974a9591aff64e47f3d6c2f190c12d085ecc9e74982101c4cc2fce8d26c1f8d785e7a6a6001d6504545182af023162f2a905a1629eb281cc9758c29d'
